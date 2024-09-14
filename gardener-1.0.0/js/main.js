d5a85edf977bec1c46383d5df4f4496d009a5670
SuhaKrasneh <suha.kras967@gmail.com>
2024-09-14 20:21:45 +0300

    update

--- gardener-1.0.0/js/main.js
+++ gardener-1.0.0/js/main.js
@@ -106,3 +106,35 @@
       });
     });
  
+// join us// Toggle the popup when the Join Us button is clicked
+document.getElementById('joinUsBtn').addEventListener('click', function() {
+    var popup = document.getElementById('joinUsPopup');
+    popup.style.display = (popup.style.display === 'block') ? 'none' : 'block';
+});
+
+
+    function countdown() {
+      const deadline = new Date("Dec 31, 2024 23:59:59").getTime();
+      const now = new Date().getTime();
+      const distance = deadline - now;
+
+    //   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    //   document.getElementById("days").innerHTML = days;
+      document.getElementById("hours").innerHTML = hours;
+      document.getElementById("minutes").innerHTML = minutes;
+      document.getElementById("seconds").innerHTML = seconds;
+
+      if (distance < 0) {
+        // document.getElementById("days").innerHTML = "0";
+        document.getElementById("hours").innerHTML = "0";
+        document.getElementById("minutes").innerHTML = "0";
+        document.getElementById("seconds").innerHTML = "0";
+      }
+    }
+
+    setInterval(countdown, 1000);
+  
